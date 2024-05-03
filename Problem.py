e9290ee0a5de7e2f8ffecc967ff8b4520b2558f3
Daniel Coelho <dcoelho045@gmail.com>
2024-05-02 20:05:35 -0700

    non working code

---
+++ Problem.py
@@ -0,0 +1,12 @@
+class Problem:
+    def __init__(self, initial_state, goal_state, operators):
+        self.initial_state = initial_state
+        self.goal_state = goal_state
+        self.operators = operators
+
+    def goal_test(self, state):
+        return state == self.goal_state
+    
+    def expand(self, node):
+        expanded_nodes = []
+        return expanded_nodes
--- main.py
+++ main.py
@@ -1,1 +1,59 @@
-print("hello world")
+from Node import Node
+from Problem import Problem
+from queue import PriorityQueue
+
+# Uniform Cost Search
+def uniform_cost_search(problem):
+    nodes = PriorityQueue()
+    nodes.put((0, Node(problem.initial_state)))  # (priority, node)
+    while not nodes.empty():
+        _, node = nodes.get()
+        if problem.goal_test(node.state):
+            return node  # Solution found
+        for child in problem.expand(node):
+            nodes.put((child.path_cost, child))
+    return "failure"  # No solution found
+
+# A* Misplaced Tile Heuristic
+def misplaced_tile_heuristic(state, goal_state):
+    return 
+    
+def astar_misplaced_tile(problem):
+    return 
+
+# A* Euclidean Distance Heuristic
+def euclidean_distance_heuristic(state, goal_state):
+    return 
+
+def astar_euclidean_distance(problem):
+    return 
+
+
+
+def main():
+    print("Welcome to dcoel003, abrem005, jgonz671, 8 puzzle solver.")
+
+    initalState = [[1,2,3],[4,8,0],[7,6,5]]
+    goalState = [[1,2,3],[4,5,6],[7,8,0]]
+    operators = ['up', 'down', 'left', 'right']
+    problem = Problem(initalState, goalState, operators)
+    print(initalState[0], '\n', initalState[1], '\n', initalState[2])
+
+    solutionNode = uniform_cost_search(problem)
+
+    if (solutionNode != "failure"):
+        print("solution node isn't a failure")
+
+        # Print the path from the initial state to the goal state
+        path = []
+        while solution_node:
+            path.append(solution_node.state)
+            solution_node = solution_node.parent
+        path.reverse()
+        for state in path:
+            print(state)
+    else:
+        print("No solution found.")
+
+if __name__ == "__main__":
+    main()
