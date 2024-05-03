fee4bdddcc14e2e9ca49251410bc38c10db99762
Julian Gonzalez <Julian@julian.eg2003@gmail.com>
2024-05-02 20:27:01 -0700

    julian's useless work

--- Node.py
+++ Node.py
@@ -4,3 +4,27 @@ class Node:
         self.parent = parent
         self.action = action
         self.heuristic = heuristic
+
+    def get_state(self):
+        return self.state 
+
+    def get_parent(self):
+        return self.parent
+
+    def get_action(self):
+        return self.action
+
+    def get_heuristic(self):
+        return self.heuristic
+
+    def set_state(self, state):
+        self.state = state
+
+    def set_parent(self, parent):
+        self.parent = parent
+
+    def set_action(self, action):
+        self.action = action
+
+    def set_heuristic(self, heuristic):
+        self.heuristic = heuristic
